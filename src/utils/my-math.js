768a6933a38d4c56c18af2cbd40accdf7acdd90e
Ma Yanbin <myanbin@gmail.com>
2020-01-06 15:11:12 +0800

    增加 mocha 测试库

--- src/utils/my-math.js
+++ src/utils/my-math.js
@@ -1,3 +1,17 @@
-export function sum(a, b) {
-  return a + b;
-};
+class MyMath {
+  sum(a, b) {
+    return a + b;
+  }
+  sum_async(a, b, callback) {
+    setTimeout(function () {
+      callback(a + b);
+    }, 1000);
+  }
+  multi(a, b) {
+    return a * b;
+  }
+}
+
+// export default { MyMath };
+
+module.exports = MyMath;
---
+++ test/my-math.test.js
@@ -0,0 +1,35 @@
+let assert = require('assert');
+// import MyMath from '../src/utils/my-math';
+let MyMath = require('../src/utils/my-math');
+
+describe('测试 MyMath', function () {
+  let my;
+  // 每个测试套件执行一次
+  before(function () {
+    my = new MyMath();
+  });
+  after(function () {
+    // console.log('测试结束');
+  });
+  // 每个测试用例执行一次
+  beforeEach(function () {
+  });
+  afterEach(function () {
+  });
+  it('测试加法', function(done) {
+    let m1 = my.sum(1, 2);
+
+    assert.ok(m1 == 3);
+
+    my.sum_async(2, 4, function (m2) {
+      assert.ok(m2 == 6);
+      // 使用 done 表示异步函数执行完成
+      done();
+    })
+  });
+  it('测试乘法', function() {
+    let m1 = my.multi(1, 2);
+
+    assert.ok(m1 == 2);
+  });
+});
